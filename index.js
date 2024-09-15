81c0bebee724f611dc193c4bdaf7c2aa8c6f66a7
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-16 02:27:01 +0600

    added get method

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
 
         const touristSpotCollection = client.db('touristSpotDB').collection('touristSpot');
 
+        app.get('/touristSpot', async(req,res) => {
+            const cursor = touristSpotCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         app.post('/touristSpot', async(req,res) => {
             const newSpot = req.body;
             console.log(newSpot);

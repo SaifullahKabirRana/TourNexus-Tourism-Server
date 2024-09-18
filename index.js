4be4ac2ec50585de2aef5baf954790c2ce440c12
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-19 02:11:22 +0600

    added findOne operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,13 @@ async function run() {
         app.get('/touristSpot', async(req,res) => {
             const cursor = touristSpotCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/touristSpot/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await touristSpotCollection.findOne(query);
             res.send(result);
         })
 
